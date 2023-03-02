40ecdf82039a6a67e6e5d6a63d8dfe46a6bcd78c
Leo-vF <vonfoerster.leo+github@gmail.com>
2023-03-02 23:29:47 +0100

    comments and autoformatting

--- stringbuilder.c
+++ stringbuilder.c
@@ -1,11 +1,12 @@
+#include "stringbuilder.h"
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include "stringbuilder.h"
 
 void init_stringbuilder(struct Stringbuilder *stringbuilder) {
     stringbuilder->string = calloc(10, 1);
-    if(stringbuilder->string == NULL){
+    if (stringbuilder->string == NULL) {
         printf("calloc error\n");
         exit(EXIT_FAILURE);
     }
@@ -13,11 +14,13 @@ void init_stringbuilder(struct Stringbuilder *stringbuilder) {
     stringbuilder->current_length = 0;
 }
 
-void append(struct Stringbuilder *stringbuilder, char src){
-    if(stringbuilder->current_length >= stringbuilder->size - 1){
-        int newSize = stringbuilder->size/2 + stringbuilder->size;
-        char*  tmp = calloc(newSize, 1);
-        if(tmp == NULL){
+void append(struct Stringbuilder *stringbuilder, char src) {
+    // sizing up the string builders buffer, in case it is not
+    // long enough to contain the string after appending
+    if (stringbuilder->current_length >= stringbuilder->size - 1) {
+        int newSize = stringbuilder->size / 2 + stringbuilder->size;
+        char *tmp = calloc(newSize, 1);
+        if (tmp == NULL) {
             printf("calloc error\n");
             exit(EXIT_FAILURE);
         }
@@ -26,27 +29,21 @@ void append(struct Stringbuilder *stringbuilder, char src){
         stringbuilder->string = tmp;
         stringbuilder->size = newSize;
     }
-    int i = 0;
-    while(*(stringbuilder->string + i)){
-        i++;
-    }
-    if (stringbuilder->current_length != i){
-        printf("\n|||str(%d) und i(%d) sind ungleich\n", stringbuilder->current_length, i);
-    }
     *(stringbuilder->string + stringbuilder->current_length) = src;
     stringbuilder->current_length++;
 }
 
-void append_string(struct Stringbuilder *stringbuilder, char* src){
-    while (*src){
+void append_string(struct Stringbuilder *stringbuilder, char *src) {
+    while (*src) {
         append(stringbuilder, *src);
         src++;
     }
 }
 
-void append_num(struct Stringbuilder *stringbuilder, long number){
-    char* number_as_string = calloc(20, 1); // TODO find way tu reduce the 20 etc.
-    if(number_as_string == NULL){
+void append_num(struct Stringbuilder *stringbuilder, long number) {
+    char *number_as_string = calloc(20, 1);
+    // TODO find way t0 reduce the 20 etc.
+    if (number_as_string == NULL) {
         printf("calloc error\n");
         exit(EXIT_FAILURE);
     }
@@ -55,7 +52,7 @@ void append_num(struct Stringbuilder *stringbuilder, long number){
     free(number_as_string);
 }
 
-//TODO make it that the returned string is smaller etc.
-char* getString(struct  Stringbuilder *stringbuilder){
+// TODO make it that the returned string is smaller etc.
+char *getString(struct Stringbuilder *stringbuilder) {
     return stringbuilder->string;
 }
